edea89f062052cc5d2c6e76a36bc9344fc5733cb
pdevops29 <118766841+pdevops29@users.noreply.github.com>
2023-01-30 12:00:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,5 @@
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  location = "East US "
 }
 
-
-
-resource "azurerm_storage_account" "example" {
-  name                     = "testxyzzstoragepraveen"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
