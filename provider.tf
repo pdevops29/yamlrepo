a79c3512392ed9a600122536e3aee9ac4540fcf0
pdevops29 <118766841+pdevops29@users.noreply.github.com>
2023-01-30 11:58:47 -0500

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "967c3412-3684-4589-aaac-87f200cbadc2"
+  client_id       = "c26cc8ef-eab6-4ae9-9f79-f2e33f7f6d15"
+  client_secret   = "HiP8Q~M18rw~KWSu2EiWvEQyFBdhPOztOnNZkb0Y"
+  tenant_id       = "68540a6a-7865-4c3d-9dba-f1f5dfc1116f"
+}
